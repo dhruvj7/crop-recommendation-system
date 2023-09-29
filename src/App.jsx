b59fe272924ae9e3eefe7e7bfab7979adc85597e
dhruvj7 <dhruvjain076@outlook.com>
2023-09-29 15:22:55 +0530

    crop page completed

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,14 @@ function App() {
   return (
     <>
       <Navigation />
-      {/* <Crop /> */}
-      <Routes>
+      <Crop />
+      {/* <Routes>
         <Route path='/' element={<Homepage />}></Route>
         <Route path='/predict' element={<PredictForm/>}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/crop' element={<Crop/>}></Route>
-      </Routes>
+      </Routes> */}
       <Footer />
     </>
   )
